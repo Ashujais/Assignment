137ce4b8c9dd117a6fb9488aea47c8376b012911
Ashutosh Kumar Jaiswal <120046706+Ashujais@users.noreply.github.com>
2024-06-16 17:37:44 +0530

    Add files via upload

---
+++ Lecture_4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=3;
+    int b=5;
+
+    //a=3;
+    //b=5;
+
+    int c;
+    c=a+b;
+
+    cout<<c;
+    cout<<endl;
+    cout<<"a+b";
+
+}
