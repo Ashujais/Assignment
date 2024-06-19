8f73a8b8507458c8a874a851c6740bfdff1fb068
Ashutosh Kumar Jaiswal <120046706+Ashujais@users.noreply.github.com>
2024-06-19 12:09:21 +0530

    Add files via upload

---
+++ Lecture_5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cout<<"Input 4 integer a,b,c and d"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b && c>d);
+    cout<<(a>b || c>d);
+
+}
