fbacda4be3d8a0b40feac6d472b4adbe0d0bf9e4
Ashutosh Kumar Jaiswal <120046706+Ashujais@users.noreply.github.com>
2024-07-16 12:31:22 +0530

    Add files via upload

---
+++ Lecture_27.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string a;
+    string b;
+    string c;
+
+    // int a;
+    // int b;
+    // int c;
+
+    getline(cin,a);
+
+    cout<<"Your full name is: "<<a<<endl;
+
+    // c=a+b;
+
+    // cout<<"The combined word is: "<<c;
+    // cout<<"The length of the combined name is "<<c.length();
+
+    return 0;
+
+}
