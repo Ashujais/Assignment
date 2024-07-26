625781470bc3e744478ea9ffec98bac244576415
Ashutosh Kumar Jaiswal <120046706+Ashujais@users.noreply.github.com>
2024-07-26 10:47:55 +0530

    Add files via upload

---
+++ Lecture_50.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+    string name_1;
+    int mids_1;
+    float ends_1;
+    bool pre_abs_1;
+
+}
