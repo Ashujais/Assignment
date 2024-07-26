bb310b4a9cc9cc9c41c9e9bbbaf8fb747a0f2c00
Ashutosh Kumar Jaiswal <120046706+Ashujais@users.noreply.github.com>
2024-07-27 02:20:07 +0530

    Add files via upload

---
+++ Lecture_51.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    public:
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+    
+};
+
+class vehicle
+{
+    public:
+
+    string car_name;
+    string tyre_name;
+    int number_of_tyres;
+    int number_of_seats;
+
+};
+
+int main()
+{
+    student a,b,c;
+
+    int d,e,f;
+
+    vehicle hyudni, suzuki, ferrari;
+
+}
